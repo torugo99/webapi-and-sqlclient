3933d15f5321f25d6e8ac6ab18b8af10e1ffeb4d
Victor Hugo <victormotabatista@gmail.com>
2023-03-28 01:00:31 -0300

    feat: updating the entities

--- Models/UserEntity.cs
+++ Models/UserEntity.cs
@@ -18,5 +18,8 @@ namespace WebApi.Models
 
         [JsonPropertyName("created_at")]
         public DateTime CreatedAt { get; set; }
+
+        [JsonPropertyName("updated_at")]
+        public DateTime UpdatedAt { get; set; }
     }
 }
