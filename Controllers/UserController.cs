d5d155b8bd22a4472e7b6d9e1a2ce1611d86c08e
Victor Hugo <victormotabatista@gmail.com>
2023-03-28 00:49:18 -0300

    feat: creating the client controller and implementing CRUD

---
+++ Controllers/UserController.cs
@@ -0,0 +1,174 @@
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{   
+    [ApiController]
+    [Route("[controller]")]
+    [Consumes("application/json")]
+    [Produces("application/json")]
+    public class UserController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public UserController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public JsonResult Post(User user)
+        {
+            string query = @"
+                           insert into dbo.Users
+                           (user_id, name_user, email, created_at)
+                           values (@user_id, @name_user, @email, @created_at)
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("database");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {   
+                    myCommand.Parameters.AddWithValue("@user_id", user.UserId = Guid.NewGuid());
+                    myCommand.Parameters.AddWithValue("@name_user", user.Name);
+                    myCommand.Parameters.AddWithValue("@email", user.Email);
+                    myCommand.Parameters.AddWithValue("@created_at", user.CreatedAt = DateTime.Now);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Created User");
+        }
+
+        [HttpGet("get-all")]
+        public JsonResult Get()
+        {
+            string query = @"
+                            select 
+                                user_id, 
+                                name_user, 
+                                email, 
+                                created_at, 
+                                updated_at 
+                            from
+                                dbo.Users
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("database");
+            SqlDataReader myReader;
+            using(SqlConnection myCon=new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using(SqlCommand myCommand=new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpGet("get-by/{user_id}")]
+        public JsonResult GetById(string user_id)
+        {
+            string query = @"
+                            select * from
+                            dbo.Users
+                            where user_id=@user_id
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("database");
+            SqlDataReader myReader;
+            using(SqlConnection myCon=new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using(SqlCommand myCommand=new SqlCommand(query, myCon))
+                {   
+                    myCommand.Parameters.AddWithValue("@user_id", user_id);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpPut("{user_id}")]
+        public JsonResult Put(string user_id, User user)
+        {
+            string query = @"
+                           update dbo.Users
+                           set 
+                            name_user=@name_user,
+                            email=@email,
+                            updated_at=@updated_at
+                            where user_id=@user_id
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("database");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@user_id", user_id);
+                    myCommand.Parameters.AddWithValue("@name_user", user.Name);
+                    myCommand.Parameters.AddWithValue("@email", user.Email);
+                    myCommand.Parameters.AddWithValue("@updated_at", user.UpdatedAt = DateTime.Now);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Updated User");
+        }
+
+        [HttpDelete("{user_id}")]
+        public JsonResult Delete(string user_id)
+        {
+            string query = @"
+                           delete from dbo.Users
+                            where user_id=@user_id
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("database");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@user_id", user_id);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted user");
+        }
+    }
+}
