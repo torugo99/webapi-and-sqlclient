eaab8f34be2ab0738433f3dd487a4020f592abd3
Victor Hugo <victormotabatista@gmail.com>
2023-03-28 05:38:21 -0300

    sql

--- Sql/creating_the_tables.sql
+++ Sql/creating_the_tables.sql
@@ -13,6 +13,8 @@ CREATE TABLE clients
     name_client VARCHAR (50) NOT NULL,
     user_id UNIQUEIDENTIFIER NULL,
     created_at DATETIME,
+    updated_at DATETIME,
     FOREIGN KEY (user_id) 
     REFERENCES users (user_id)
+    ON DELETE SET NULL
 );
